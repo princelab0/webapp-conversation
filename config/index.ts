e5f953ba632a2e10bf2dd65e3f8e24d35a6b505f
Princelab Pvt. Ltd. <115681950+princelab0@users.noreply.github.com>
2024-11-26 11:13:44 +0545

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,10 +1,10 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = '164a762b-36d8-4e78-83c4-cf027aa2664e' 
-export const API_KEY = 'app-gQK2rqx46zO34eXOgQzDstKh'
+export const APP_ID = 'c3aae34a-3a27-4f0a-a7b5-f6a9985a4116' 
+export const API_KEY = 'app-gXwLN5huGddMEnIFqaCwmBaN'
 export const API_URL = 'https://api.dify.ai/v1'
 
 export const APP_INFO: AppInfo = {
-  title: 'Nabil bank',
+  title: 'Global IME',
   description: '',
   copyright: '',
   privacy_policy: '',
